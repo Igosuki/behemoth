97898cd58fc76059981a574e90100c90367f5e78
Aurélien Maury <amaury@xebia.fr>
2013-01-30 23:28:52 +0100

    woohoo

--- web/web-app/js/ng/controllers/BookCtrl.js
+++ web/web-app/js/ng/controllers/BookCtrl.js
@@ -4,6 +4,8 @@ function BookCtrl($scope, $route, $routeParams, $location, Book) {
     $scope.$location = $location;
     $scope.$routeParams = $routeParams;
 
+    $scope.bookList = Book.query();
+
     $scope.save = function (item) {
         new Book(item).$save(function(savedObject, reponseHttpHeader) {
             $location.path('/books');
