cf0d6a8a623c7fdb03dffd05486f768497b603ae
Bastien Caudan <bastien.caudan@gmail.com>
2013-02-06 16:12:24 +0100

    Like des artistes depuis la recherche

--- web/web-app/js/ng/controllers/SearchController.js
+++ web/web-app/js/ng/controllers/SearchController.js
@@ -8,13 +8,20 @@ function SearchController($scope, $route, $routeParams, $location, $resource) {
         $location.path($routeParams.domain + '/artist/' + artistId);
     };
 
+    $scope.like = function(artistId){
+        var Checker = $resource(CTX + '/userPreference/likeArtist',
+            {id: artistId},
+            {});
+        Checker.save();
+    };
+
     $scope.search = function() {
         if ($scope.complete !== undefined && $scope.complete != '') {
-            var Artist = $resource('/web/artists/complete', {},
+            var Artist = $resource(CTX + '/artists/complete', {},
                 {complete: {method: 'GET', isArray: true, params: {'start': $scope.complete}}});
             $scope.artistList = Artist.complete();
 
-            var Song = $resource('/web/songs/complete', {},
+            var Song = $resource(CTX + '/songs/complete', {},
                 {complete: {method: 'GET', isArray: true, params: {'start': $scope.complete}}});
             $scope.songList = Song.complete();
         }
