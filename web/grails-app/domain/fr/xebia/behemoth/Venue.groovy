b80c0e78a63db5c45ec39d7e122546d93a13f4f4
Stéphane Moreau <smoreau@xebia.fr>
2013-02-06 15:01:37 +0100

    Action search is now returning all of sort of informations

---
+++ web/grails-app/domain/fr/xebia/behemoth/Venue.groovy
@@ -0,0 +1,11 @@
+package fr.xebia.behemoth
+
+class Venue {
+    String id
+    String url
+    String name
+    String address
+
+    static constraints = {
+    }
+}
--- web/grails-app/utils/fr/xebia/behemoth/EventsBuilder.groovy
+++ web/grails-app/utils/fr/xebia/behemoth/EventsBuilder.groovy
@@ -10,9 +10,32 @@ class EventsBuilder {
         def events = []
 
         data.events[0].event.each {
+            String startTime = it.start_time.text()
+            String stopTime = it.stop_time.text()
+
             Event event = new Event()
             event.title = it.title
             event.url = it.url
+            event.description = it.description
+            if (!startTime.isEmpty()) {
+                event.startDate = new Date().parse("yyyy-M-d H:m:s", startTime)
+            }
+            if (!stopTime.isEmpty()) {
+                event.endDate = new Date().parse("yyyy-M-d H:m:s", stopTime)
+            }
+            event.city = it.city_name
+            event.country = it.country_name
+            event.latitude = it.latitude.text().toFloat()
+            event.longitude = it.longitude.text().toFloat()
+            event.imageUrl = it.image.url
+
+            Venue venue = new Venue()
+            venue.id = it.venue_id
+            venue.url = it.venue_url
+            venue.name = it.venue_name
+            venue.address = it.venue_address
+            event.venue = venue
+
             events.add(event)
         }
 
