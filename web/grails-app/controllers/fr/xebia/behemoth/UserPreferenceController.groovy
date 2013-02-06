8503b421bc1d640dfb229e13d3b377dbe2c65cdf
Pablo Lopez <plopez@xebia.fr>
2013-02-06 14:33:48 +0100

    Enrichissement du controler User

--- web/grails-app/controllers/fr/xebia/behemoth/UserPreferenceController.groovy
+++ web/grails-app/controllers/fr/xebia/behemoth/UserPreferenceController.groovy
@@ -4,15 +4,19 @@ class UserPreferenceController {
 
     def badgeService
     def springSecurityService
+    def allUsersActionsService
 
     def listArtists() {
-        return springSecurityService.currentUser.favouriteArtists
+        def result  = springSecurityService.currentUser.favouriteArtists
+        // TODO render page
     }
 
     def addArtists(Artist artist) {
+        allUsersActionsService.registerEvent(new AllUsersEvent(springSecurityService.currentUser.username, "a ajouté "+ artist.name))
         springSecurityService.currentUser.favouriteArtists << artist
         def newBadges = badgeService.checkBadgeOnAdd(artist);
-        // Save user
+        // TODO save user
+        // TODO render page
     }
 
 
