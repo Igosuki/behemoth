dc61383df275638218f070b0d253d609e57c98c8
Aurélien Maury <amaury@xebia.fr>
2013-02-06 15:09:02 +0100

    profile start

--- web/web-app/js/ng/scaffolding.js
+++ web/web-app/js/ng/scaffolding.js
@@ -29,6 +29,7 @@ scaffoldingModule.config([
             when('/map', { templateUrl: baseUrl + 'imap/active_map.html', controller: MapController}).
             when('/home', { templateUrl: baseUrl + '/home.html' }).
             when('/search', { templateUrl: baseUrl + '/search.html' }).
+            when('/profile', { templateUrl: baseUrl + '/profile.html' }).
             when('/', {redirectTo: '/home'} ).
             when('/map', { templateUrl: baseUrl + 'imap/active_map.html', controller: MapController}).
             when('/login', { templateUrl: baseUrl + 'login.html', controller: LoginController}).
