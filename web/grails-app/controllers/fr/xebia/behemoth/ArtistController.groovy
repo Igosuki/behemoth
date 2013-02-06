027e5def671c2769e2953413e8ee8e767dfea190
Stéphane Moreau <smoreau@xebia.fr>
2013-02-06 11:32:19 +0100

    Artist controller with the complete method

---
+++ web/grails-app/controllers/fr/xebia/behemoth/ArtistController.groovy
@@ -0,0 +1,50 @@
+package fr.xebia.behemoth
+
+import grails.converters.JSON
+import static javax.servlet.http.HttpServletResponse.*
+
+class ArtistController {
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index() {}
+
+    def list() {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+        response.setIntHeader('X-Pagination-Total', Artist.count())
+        render Artist.list(params) as JSON
+    }
+
+
+    def get() {
+        def artistInstance = Artist.get(params.id)
+        if (artistInstance) {
+            render artistInstance as JSON
+        } else {
+            notFound params.id
+        }
+    }
+
+    def complete() {
+        String start = params.start ?: ''
+        Integer max = params.max ? params.max.toInteger() : 20
+        Integer offset = params.offset ? params.offset.toInteger() : 0
+
+        println max
+        println offset
+
+        def result = Artist.findAllByNameIlike(start + '%', [max: max, offset: offset, sort: 'name', order: 'asc'])
+        def finalResult = result.collect {
+            [ id: it.id, name: it.name ]
+        }
+
+        render finalResult as JSON
+    }
+
+
+    private void notFound(id) {
+        response.status = SC_NOT_FOUND
+        def responseJson = [message: message(code: 'default.not.found.message', args: [message(code: 'artist.label', default: 'Artist'), params.id])]
+        render responseJson as JSON
+    }
+}
